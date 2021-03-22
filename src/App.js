dfde639d3ed7d96aaf7cbca7dc4f53b9be30437d
Jaggehn <jaggehns@gmail.com>
2021-03-22 15:07:01 +0800

    Styled Components init

--- src/App.js
+++ src/App.js
@@ -1,10 +1,15 @@
 import React from "react";
+import GLobalStyle from "./components/GlobalStyle";
+//Global
+import GlobalStyle from "./components/GlobalStyle";
+
 //Import pages
 import AboutMe from "./pages/AboutMe";
 
 function App() {
   return (
     <div className="App">
+      <GLobalStyle />
       <AboutMe />
     </div>
   );
