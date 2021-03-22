63abe5ea7c949f01495f0a1dd61f692a237dc36f
Jaggehn <jaggehns@gmail.com>
2021-03-22 13:45:56 +0800

    FAQ Section

---
+++ src/components/FaqSection.js
@@ -0,0 +1,37 @@
+import React from "react";
+
+const FaqSection = () => {
+  return (
+    <div className="faq">
+      <h2>
+        Any Questions <span>FAQ</span>
+      </h2>
+      <div className="question">
+        <h4>Where Am I Based?</h4>
+        <div className="answer">
+          <p>Lorem ipsum dolor sit amet.</p>
+        </div>
+      </div>
+      <div className="question">
+        <h4>How Long Have I Been Coding for?</h4>
+        <div className="answer">
+          <p>Lorem ipsum dolor sit amet.</p>
+        </div>
+      </div>
+      <div className="question">
+        <h4>What Is My Background?</h4>
+        <div className="answer">
+          <p>Lorem ipsum dolor sit amet.</p>
+        </div>
+      </div>
+      <div className="question">
+        <h4>What Are My Values?</h4>
+        <div className="answer">
+          <p>Lorem ipsum dolor sit amet.</p>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default FaqSection;
--- src/pages/AboutMe.js
+++ src/pages/AboutMe.js
@@ -2,12 +2,14 @@ import React from "react";
 //Page Components
 import AboutSection from "../components/AboutSection";
 import SkillsSection from "../components/SkillsSection";
+import FaqSection from "../components/FaqSection";
 
 const AboutMe = () => {
   return (
     <>
       <AboutSection />
       <SkillsSection />
+      <FaqSection />
     </>
   );
 };
