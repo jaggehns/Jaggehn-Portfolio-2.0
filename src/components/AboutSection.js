685f7bad96c1ade7ef67e2410ce39611f75dc48b
Jaggehn <jaggehns@gmail.com>
2021-03-23 14:32:17 +0800

    Wave Animation

--- src/components/AboutSection.js
+++ src/components/AboutSection.js
@@ -5,6 +5,7 @@ import { About, Description, Image, Hide } from "../styles";
 //Framer Motion
 import { motion } from "framer-motion";
 import { titleAnim, fade, photoAnim } from "../animation";
+import Wave from "./Wave";
 
 const AboutSection = () => {
   return (
@@ -32,6 +33,7 @@ const AboutSection = () => {
       <Image>
         <motion.img variants={photoAnim} src={home1} alt="me" />
       </Image>
+      <Wave />
     </About>
   );
 };
