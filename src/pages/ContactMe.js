90412a876c50cef5477641c92697b855858fd5ab
Jaggehn <jaggehns@gmail.com>
2021-04-01 05:09:10 +0800

    Contact footer

--- src/pages/ContactMe.js
+++ src/pages/ContactMe.js
@@ -3,39 +3,78 @@ import React from "react";
 import { motion } from "framer-motion";
 import { pageAnimation } from "../animation";
 import styled from "styled-components";
+import github from "../img/github.svg";
+import instagram from "../img/instagram.svg";
+import linkedin from "../img/linkedin.svg";
 
 const ContactMe = () => {
   return (
-    <ContactStyle
-      exit="exit"
-      variants={pageAnimation}
-      initial="hidden"
-      animate="show"
-      style={{ background: "#fff" }}
-    >
-      <motion.form action="https://formspree.io/f/mwkwdlke" method="POST">
-        <label htmlFor="name">Name</label>
-        <motion.input type="text" name="name" id="name" required />
-        <label htmlFor="email">Email</label>
-        <motion.input type="email" name="_replyto" id="email" required />
-        <label htmlFor="message">Message</label>
-        <motion.textarea
-          name="message"
-          id="message"
-          cols="30"
-          rows="10"
-        ></motion.textarea>
-        <motion.button value="Send" type="submit">
-          Submit
-        </motion.button>
-      </motion.form>
-    </ContactStyle>
+    <div>
+      <ContactStyle
+        exit="exit"
+        variants={pageAnimation}
+        initial="hidden"
+        animate="show"
+        style={{ background: "#fff" }}
+      >
+        <motion.form action="https://formspree.io/f/mwkwdlke" method="POST">
+          <label htmlFor="name">Name</label>
+          <motion.input type="text" name="name" id="name" required />
+          <label htmlFor="email">Email</label>
+          <motion.input type="email" name="_replyto" id="email" required />
+          <label htmlFor="message">Message</label>
+          <motion.textarea
+            name="message"
+            id="message"
+            cols="30"
+            rows="10"
+          ></motion.textarea>
+          <motion.button value="Send" type="submit">
+            Submit
+          </motion.button>
+        </motion.form>
+      </ContactStyle>
+      <Footer>
+        <motion.footer>
+          {/* <motion.h4>Jaggehn 2021</motion.h4> */}
+          <motion.ul>
+            <motion.li>
+              <a
+                href="https://github.com/jaggehns"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                <motion.img src={github} alt="twitter-social" />
+              </a>
+            </motion.li>
+            <motion.li>
+              <a
+                href="https://www.instagram.com/jaggehn_"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                <motion.img src={instagram} alt="instagram-social" />
+              </a>
+            </motion.li>
+            <motion.li>
+              <a
+                href="https://www.linkedin.com/in/jaggehn-sivabalan/"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                <motion.img src={linkedin} alt="linkedin-social" />
+              </a>
+            </motion.li>
+          </motion.ul>
+        </motion.footer>
+      </Footer>
+    </div>
   );
 };
 
 const ContactStyle = styled(motion.div)`
   box-sizing: border-box;
-  min-height: 90vh;
+  min-height: 78vh;
   display: flex;
   align-items: center;
   justify-content: center;
@@ -63,10 +102,43 @@ const ContactStyle = styled(motion.div)`
   button {
     width: 100%;
     padding: 2rem;
-    background: #154353;
+    background: #282828;
+    border: 2px solid;
     color: white;
     border-radius: 1rem;
   }
+`;
+
+const Footer = styled(motion.div)`
+  position: absolute;
+  width: 100%;
+
+  footer {
+    background: #282828;
+    color: white;
+    min-height: 10vh;
+    padding: 0% 5%;
+    display: flex;
+    justify-content: start;
+    align-items: center;
+    flex-direction: row;
+  }
+
+  ul {
+    flex: 1 1 40rem;
+    display: flex;
+    justify-content: space-around;
+    flex-direction: row;
+    list-style: none;
+  }
+
+  /* h4 {
+    flex: 1 1 40rem;
+    font-size: 1.5rem;
+    font-family: "Lobster", cursive;
+    font-weight: lighter;
+  } */
+
   @media (max-width: 1500px) {
     form {
       padding: 0rem;
